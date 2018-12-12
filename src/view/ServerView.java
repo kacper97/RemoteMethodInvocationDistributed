bca539b53c5fbfd71f9c2482a28f57c430bd9dca
unknown <kacperwoloszyn4@gmail.com>
2018-12-12 00:25:26 +0000

    From previous assignment implementation of new window

--- src/view/ServerView.java
+++ src/view/ServerView.java
@@ -1,7 +1,43 @@
 package view;
 
-public class ServerView {
+import controller.CalculatorServer;
+import model.Calculator;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class ServerView implements ActionListener {
 	
 	// Server View will provide a text output for calculator
+	final JTextArea area = new JTextArea();
+    final JButton calcBtn;
 
+    public ServerView(){
+        JFrame frame = new JFrame("RMI Server");
+        calcBtn = new JButton("Calculator");
+
+        calcBtn.addActionListener(this);
+
+        frame.add(new JScrollPane(area), BorderLayout.CENTER);
+        frame.add(calcBtn, BorderLayout.SOUTH);
+
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setSize(500, 300);
+        frame.setVisible(true);
+    }
+
+    public void handleAction(String s){
+        area.append(s + '\n');
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String action = e.getActionCommand();
+        if (action.equals("Calculator")){
+            new CalculatorView();
+        }
+    }
 }
