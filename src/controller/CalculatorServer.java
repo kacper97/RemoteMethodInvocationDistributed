8c0ef48a1f474c45802d21bc4c1842c0af412190
unknown <kacperwoloszyn4@gmail.com>
2018-12-13 20:17:52 +0000

    More comments

--- src/controller/CalculatorServer.java
+++ src/controller/CalculatorServer.java
@@ -27,32 +27,41 @@ public CalculatorServer() throws RemoteException {
 
 @Override
 public double add(double x, double y) throws RemoteException {
-	System.out.println("Add Nums bound in registry");
-	serverView.handleAction("Add called : " + x + "+ " + y +"\n The following  answer is passed to client : " + (x+y));
+	serverView.handleAction("Add called : " + x + "+ " + y);
+	serverView.handleAction("Calculating .........");
+	serverView.handleAction("The following  answer is passed to client : " + (x+y));
 	return x + y ;
 }
 
 @Override
 public double sub(double x, double y) throws RemoteException {
-	serverView.handleAction("Sub Called called : " + x + "- " + y +"\n The following numbers and operators passed to client : " + (x-y));
+	serverView.handleAction("Sub Called called : " + x + "- " + y);
+	serverView.handleAction("Calculating .........");
+	serverView.handleAction("The following numbers and operators passed to client : " + (x-y));
 	return x- y ;
 }
 
 @Override
 public double multiply(double x, double y) throws RemoteException {
-	serverView.handleAction("Mult called : " + x + "* " + y +"\n The following answer is passed to client : " + (x*y));
+	serverView.handleAction("Mult called : " + x + "* " + y);
+	serverView.handleAction("Calculating .........");
+	serverView.handleAction("The following answer is passed to client : " + (x*y));
 	return x*y;
 }
 
 @Override
 public double divide(double x, double y) throws RemoteException {
-	serverView.handleAction("Div called : " + x + "/ " + y +"\n The following answer is passed to client : " + (x/y));
+	serverView.handleAction("Div called : " + x + "/ " + y);
+	serverView.handleAction("Calculating .........");
+	serverView.handleAction("The following answer is passed to client : " + (x/y));
 	return x/y;
 }
 
 @Override
 public double power(double x, double y) throws RemoteException {
-	serverView.handleAction("Power called : " + x + "^ " + y +"\n The following answer is passed to client : " + (Math.pow(x,y)));
+	serverView.handleAction("Power called : " + x + "^ " + y );
+	serverView.handleAction("Calculating .........");
+	serverView.handleAction(" The following answer is passed to client : " + (Math.pow(x,y)));
 	return Math.pow(x,y);
 }
 
@@ -72,7 +81,7 @@ public static void main(String args[]) {
 	
 		//Naming.rebind("HelloWorld", obj);
 		System.out.println("Calculator bound in registry");
-
+		serverView.handleAction("Calculator is bound in registery");
 	}
 	catch (Exception e) {
 		System.out.println("Calculator error: " + e.getMessage());
